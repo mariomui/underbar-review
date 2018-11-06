28afe1642cf38c7fd90b6a41bca28f9c56c6a95c
mariomui <yi.kan.mui@gmail.com>
2018-11-06 10:30:58 -0800

    First Commit

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -31,12 +32,23 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if ( n === 0 ) {
+      return [];
+    }
     return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    const end = array.length -1;
+    if ( n > end + 1) {
+      return array;
+    }
+    if ( n === 0 ) {
+      return [];
+    }
+    return n === undefined ? array[end] : array.slice(-n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +57,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for ( var key in collection) {
+        iterator( collection[key] , key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
